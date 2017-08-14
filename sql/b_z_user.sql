4196d6ecfe6bc72d3195d0f9da71b6bfdcad8099
Bigbin212 <liubin_121@126.com>
2017-08-14 13:54:47 +0800

    脚本修改

--- sql/b_z_user.sql
+++ sql/b_z_user.sql
@@ -23,4 +23,8 @@ CREATE TABLE `b_z_user` (
 -- ----------------------------
 -- Records of b_z_user
 -- ----------------------------
-INSERT INTO `b_z_user` VALUES ('c9cc9174d9894fee92223266df36f649', 'admin', '930109', '172.16.192.171', 'xxxxx@qq.com', '2016-04-29 11:09:46', '13863336458', null, '0');
+--管理员
+INSERT INTO `b_z_user` VALUES ('c9cc9174d9894fee92223266df36f649', 'admin', '930109', '127.0.0.1', 'admin@qq.com', null, null, null, '0');
+--普通用户
+INSERT INTO `b_z_user` VALUES ('d931b50f1db44ee197e175c9d217c37b', 'godness', '888888', '127.0.0.1', 'godness@qq.com', null, null, NULL, '1');
+commit;
